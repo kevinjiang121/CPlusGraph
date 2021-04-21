0c4251e9ebcdcae2c187962deb6dfc49d73c2e7b
Kevin Jiang <kevinjiang121@gmail.com>
2021-04-21 01:13:22 -0400

    Initial commit, files added by CLion

---
+++ cmake-build-debug/CMakeFiles/CPlusGraph.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPlusGraph.dir/main.cpp.obj"
+  "CPlusGraph.exe"
+  "CPlusGraph.exe.manifest"
+  "CPlusGraph.pdb"
+  "libCPlusGraph.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPlusGraph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
