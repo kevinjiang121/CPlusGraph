1ddb7169d4d33c4ae9d592eb9ae5e250c0d5cfc5
Kevin Jiang <kevinjiang121@gmail.com>
2021-04-22 19:09:45 -0400

    Added GraphNode.cpp

--- GraphNode.cpp
+++ GraphNode.cpp
@@ -1,14 +1,13 @@
 //include statements
 #include "GraphNode.h"
 
-/*
 GradeNode::GraphNode() {
 
 }
 
 GraphNode::~GraphNode() {
 
-}*/
+}
 
 void GraphNode::print() {
     cout << "Hello World" << endl;
