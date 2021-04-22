e28ba81a98fb74c717e2284d32cdd6a053a5b27f
Kevin Jiang <kevinjiang121@gmail.com>
2021-04-22 19:55:30 -0400

    Made revisions to GraphNode.cpp

--- GraphNode.cpp
+++ GraphNode.cpp
@@ -1,13 +1,14 @@
 //include statements
 #include "GraphNode.h"
 
-GradeNode::GraphNode() {
+/*
+GraphNode::GraphNode() {
 
 }
 
 GraphNode::~GraphNode() {
 
-}
+}*/
 
 void GraphNode::print() {
     cout << "Hello World" << endl;
