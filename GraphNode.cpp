d4a64efe4877e0e4901a589ccd575900aed56732
Kevin Jiang <kevinjiang121@gmail.com>
2021-04-22 19:06:50 -0400

    Revisions made by CLion

---
+++ GraphNode.cpp
@@ -0,0 +1,16 @@
+//include statements
+#include "GraphNode.h"
+
+/*
+GradeNode::GraphNode() {
+
+}
+
+GraphNode::~GraphNode() {
+
+}*/
+
+void GraphNode::print() {
+    cout << "Hello World" << endl;
+}
+
---
+++ GraphNode.h
@@ -0,0 +1,26 @@
+//include statements
+#include <iostream>
+#include <string>
+
+//header guards
+#ifndef CPLUSGRAPH_GRAPHNODE_H
+#define CPLUSGRAPH_GRAPHNODE_H
+
+//namespaces
+using namespace std;
+
+//declare GradeNode class
+class GraphNode {
+public:
+    //constructors and destructors
+    //GradeNode();
+    //~GraphNode();
+
+    //methods
+    void print();
+private:
+    int node;
+};
+
+
+#endif
