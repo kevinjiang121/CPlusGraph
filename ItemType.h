91d3835938290b9a433cd0f79bd0c041158f0d84
Kevin Jiang <kevinjiang121@gmail.com>
2021-04-25 23:45:59 -0400

    Finished ItemType.h

--- ItemType.h
+++ ItemType.h
@@ -1,14 +1,26 @@
-//
-// Created by Kevin Jiang on 4/22/2021.
-//
+//include statements
+#include <iostream>
+#include <string>
 
+//header guards
 #ifndef CPLUSGRAPH_ITEMTYPE_H
 #define CPLUSGRAPH_ITEMTYPE_H
 
+//namespaces
+using namespace std;
 
+//declares ItemType class
 class ItemType {
+public:
+    //constructor
+    ItemType();
 
+    //methods
+    void initialize(string str);
+    string getValue();
+    void print();
+private:
+    string value;
 };
-
 
 #endif //CPLUSGRAPH_ITEMTYPE_H
