e9c0987396fbf77af77c8640fba65f56ec9ec7fc
Kevin Jiang <kevinjiang121@gmail.com>
2021-04-25 23:46:06 -0400

    Finished ItemType.cpp

--- ItemType.cpp
+++ ItemType.cpp
@@ -1,5 +1,18 @@
-//
-// Created by Kevin Jiang on 4/22/2021.
-//
-
+//include statements
 #include "ItemType.h"
+
+ItemType::ItemType() {
+    this->value = "";
+}//ItemType
+
+string ItemType::getValue() {
+    return this->value;
+}//getValue
+
+void ItemType::initialize(string str) {
+    this->value = str;
+}//initialize
+
+void ItemType::print() {
+    cout << this->value << endl;
+}//print
