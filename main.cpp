c012ea30aae776e6e87e02ca2f370a9dd93126c8
Kevin Jiang <kevinjiang121@gmail.com>
2021-05-02 19:26:40 -0400

    Created test cases for insert, insertHelper, printGraph, and printNode.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,18 @@ using namespace std;
 
 int main() {
     GraphNode node;
-    node.print();
+    ItemType vertex;
+    vertex.initialize("Graph");
+    node.insert(vertex, 5);
+    node.insert(vertex, 4);
+    node.insert(vertex, 3);
+    //node.printNode();
+    node.insert(vertex, 2);
+    //node.printNode();
+    node.insert(vertex,1);
+    //node.printNode();
+    node.insert(vertex,0);
+
+    node.printGraph();
     return 0;
 }
