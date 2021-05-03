1244fb38280f92d769116815d16876597035cc38
Kevin Jiang <kevinjiang121@gmail.com>
2021-05-03 18:31:08 -0400

    Added comments to finished methods in GraphNode.cpp

--- GraphNode.cpp
+++ GraphNode.cpp
@@ -10,8 +10,9 @@ GraphNode::~GraphNode() {
 }
 
 void GraphNode::insert(ItemType vertex, int degree) {
+    //call method
     insertHelper(vertex, degree);
-}
+}//insert
 
 void GraphNode::deleteItem(ItemType vertex) {
 
@@ -30,52 +31,54 @@ void GraphNode::getAdjacency() {
 }
 
 int** GraphNode::getDegreeMatrix() {
+    //instaniate degreeM
     int ** degreeM = new int*[numOfVertex];
     for(int i = 0; i<numOfVertex; i++) {
         degreeM[i] = new int[numOfVertex];
-    }
+    }//for
     currentPos = start;
+    //this nested for loops inserts values into degree matrix
     for(int i=0; i<numOfVertex; i++) {
         for(int j=0; j<numOfVertex; j++) {
-            if(i==j) {
+            if(i==j) { //only inputs degree on diagonal
                 degreeM[i][j] = currentPos->degree;
                 currentPos = currentPos->next;
             } else {
                 degreeM[i][j] = 0;
-            }
-        }
-    }
+            }//if else
+        }//for
+    }//for
     return degreeM;
-}
+}//getDegreeMatrix
 
 void GraphNode::printDegreeMatrix() {
     int ** degreeM = getDegreeMatrix();
     for(int i=0; i<numOfVertex; i++) {
         for(int j = 0; j<numOfVertex; j++) {
             cout << degreeM[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
+        }//for
+        cout << endl;//makes it look more like matrix
+    }//for
+}//printDegreeMatrix
 
 void GraphNode::insertHelper(ItemType vertex, int degree) {
     currentPos = start;
     Node *temp;
     int position = 0;
-    if(start == NULL) {
+    if(start == NULL) { //this inserts if graph if empty
         currentPos = new Node();
         currentPos->vertex = vertex;
         currentPos->next = NULL;
         currentPos->degree = degree;
         currentPos->position = 1;
         start = currentPos;
-    } else if (start->next==NULL) {
+    } else if (start->next==NULL) { //this inserts if graph only has 1 node
         currentPos = new Node();
         currentPos->vertex = vertex;
         currentPos->degree = degree;
         currentPos->position = (start->position)+1;
         start->next = currentPos;
-    } else {
+    } else { //inserts for all other cases
         while (currentPos->next != NULL) {
             currentPos = currentPos->next;
             position = currentPos->position;
@@ -86,35 +89,36 @@ void GraphNode::insertHelper(ItemType vertex, int degree) {
         temp->position = position + 1;
         currentPos->next = temp;
         numOfVertex = position + 1;
-    }
-}
+    } //if else
+}//insertHelper
 
 void GraphNode::print() {
     cout << "Hello World" << endl;
-}
+}//print
 
 void GraphNode::printGraph() {
     currentPos = start;
     if(start==NULL) {
         cout << "empty graph" << endl;
         return;
-    }
+    }//if
+    //prints through calling printNode
     printNode();
     while(currentPos->next != NULL) {
         currentPos = currentPos->next;
         printNode();
-    }
-}
+    }//while
+}//printGraph
 
 void GraphNode::printNode() {
     if(start == NULL) {
         cout << "empty graph" << endl;
     } else {
-        if(currentPos!=NULL) {
-            cout << currentPos->position << endl;
+        if(currentPos!=NULL) { //prints the degree of currnt node
+            cout << currentPos->degree << endl;
         } else {
             cout << "ERROR in printNode" << endl;
-        }
-    }
-}
+        }//if else
+    }//if else
+} //printNode
 
