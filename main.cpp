dd07de9ff3c887ded2f571cd4f093a0e24ae7a97
Kevin Jiang <kevinjiang121@gmail.com>
2021-05-06 23:12:28 -0400

    Tested new methods.

--- main.cpp
+++ main.cpp
@@ -10,13 +10,20 @@ using namespace std;
 int main() {
     GraphNode node;
     ItemType vertex;
+    //int n=2;
+    int edge1[2] = {2,6};
+    int edge2[2] = {1,3};
+    int edge3[2] = {2,4};
+    int edge4[2] = {3,5};
+    int edge5[2] = {4,6};
+    int edge6[2] = {5,1};
     vertex.initialize("Graph");
-    node.insert(vertex, 5);
-    node.insert(vertex, 4);
-    node.insert(vertex, 3);
-    node.insert(vertex, 2);
-    node.insert(vertex,1);
-    node.insert(vertex,0);
-    node.printDegreeMatrix();
+    node.insert(vertex, 2, edge1);
+    node.insert(vertex, 2, edge2);
+    node.insert(vertex, 2, edge3);
+    node.insert(vertex, 2, edge4);
+    node.insert(vertex,2, edge5);
+    node.insert(vertex,2, edge6);
+    node.printLaplacian();
     return 0;
 }
