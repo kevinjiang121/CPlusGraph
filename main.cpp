94a80c424d8468b2ffefe024be82237ecd2c3520
Kevin Jiang <kevinjiang121@gmail.com>
2021-05-23 16:06:52 -0400

    Added user input for the graph.

--- main.cpp
+++ main.cpp
@@ -9,44 +9,68 @@ using namespace std;
 
 int main() {
     GraphNode node;
-    ItemType vertex;
-    /*
     int v;
 
-    cout << "Number of vertices on the graph:  ";
+    cout << "Number of vertices on the graph: ";
     cin >> v;
+    cout << endl;
     for(int i=0; i<v; i++) {
         string data;
-        cout << "What is stored in vertex " << i+1 << ": ";
+        cout << "Value of vertex " << i + 1 << ": ";
         cin >> data;
         ItemType vertex;
         vertex.initialize(data);
-        cout << "Number of edges that vertex "<< i+1 << " has: ";
+        cout << endl;
+        cout << "Number of edges for vertex " << i + 1 << ": ";
         int e;
         cin >> e;
         int *edges = new int[e];
-        cout << "Enter the vertex/vertices that vertex" <<
-        for(int j=0; j<e; j++) {
-            int edge;
+        cout << endl;
+        cout << "Vertices connect to vertex " << i+1 << ":" << endl;
+        for (int j = 0; j < e; j++) {
+            int edge = v + 1;
+            while (edge > v) {
+                cin >> edge;
+                if (edge == i+1) {
+                    cout << "ERROR: Vertex cannot be connected with itself, please try again." << endl;
+                } else if (edge <= v) {
+                    edges[j] = edge;
+                } else {
+                    cout << "Error: Vertex does not exist, please try again." << endl;
+                }
+            }
+        }
+        node.insert(vertex, e, edges);
+    }
+    char command = 'f';
+    while(command != 'q') {
+        cout << "d - print the degree matrix, a - print adjacency matrix, l - print graph laplacian, "
+                "e - print eigenvalues of the laplacian, v - print eigenvectors of the laplacian, and q - quit" << endl;
+        cout << endl;
+        cin >> command;
+        if(command=='d') {
+            cout << "Printing degree matrix..." << endl;
+            node.printDegreeMatrix();
+            cout << endl;
+        } else if(command=='a') {
+            cout << "Printing adjaceny matrix..." << endl;
+            node.printAdjacenyMatrix();
+            cout << endl;
+        } else if(command=='l') {
+            cout << "Printing graph Laplacian..." << endl;
+            node.printLaplacian();
+            cout << endl;
+        } else if(command=='e') {
+            cout << "Not Implemented Yet" << endl;
+        } else if(command=='v') {
+            cout << "Not Implemented Yet" << endl;
+        } else if(command=='q') {
+            cout << "Exiting Program" << endl;
+            cout << endl;
+        } else {
+            cout << "Please enter valid input!" << endl;
+            cout << endl;
         }
     }
-     */
-
-    //int n=2;
-    int edge1[2] = {2,6};
-    int edge2[2] = {1,3};
-    int edge3[2] = {2,4};
-    int edge4[2] = {3,5};
-    int edge5[2] = {4,6};
-    int edge6[2] = {5,1};
-
-    vertex.initialize("Graph");
-    node.insert(vertex, 2, edge1);
-    node.insert(vertex, 2, edge2);
-    node.insert(vertex, 2, edge3);
-    node.insert(vertex, 2, edge4);
-    node.insert(vertex,2, edge5);
-    node.insert(vertex,2, edge6);
-    node.printLaplacian();
     return 0;
 }
